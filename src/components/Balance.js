5b2cbea8214e79cb3e4cf075720dbe72c6ff14c1
Maleeha Khalid <maleehakhalid604@gmail.com>
2020-06-10 22:22:32 +0500

    Changing class to className and removing ids

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -4,7 +4,7 @@ export const Balance = () => {
     return (
         <div>
             <h3>Your Balance</h3>
-            <h1 id="balance">$0.00</h1>
+            <h1>$0.00</h1>
         </div>
     )
 }
